3cfe211f53bdc28ca78ff560d285a9b00009b970
AlexeyLavrishin <alexeylavrishin@gmail.com>
2024-11-06 22:30:14 +0300

    feat: add button and icons components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import ButtonComponent from './shared/button/button.component';
+import SettingsIconComponent from './shared/icons/settings-icon/settings-icon.component';
+import UserIconComponent from './shared/icons/user-icon/user-icon.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonComponent, SettingsIconComponent, UserIconComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
